ef92c30aa8318728a79de6862fe79a70db4f1f44
Kyle Evans <krevan88@protonmail.com>
2022-02-25 12:05:02 -0700

    tables, models and env up to date

---
+++ FindersKeeprs/Models/Vault.cs
@@ -0,0 +1,12 @@
+namespace FindersKeeprs.Models
+{
+  public class Vault
+    {
+        public int Id { get; set; }
+        public string CreatorId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool? isPrivate { get; set; }
+        public Profile Creator { get; set; }
+    }
+}
