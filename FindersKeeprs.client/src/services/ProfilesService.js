055c675c1e171205f71ddd942ba58cb01ffddabe
Kyle Evans <krevan88@protonmail.com>
2022-03-04 08:58:36 -0700

    fixed bad pull

--- FindersKeeprs.client/src/services/ProfilesService.js
+++ FindersKeeprs.client/src/services/ProfilesService.js
@@ -6,21 +6,22 @@ import { api } from "./AxiosService"
 class ProfilesService {
   async getProfileById(userId) {
     const res = await api.get('api/profiles/' + userId)
-    logger.log(res.data)
+    // logger.log("Profile", res.data)
     AppState.activeProfile = res.data
   }
 
   async getKeepsByProfileId(userId) {
-    const res = await api.get('api/profiles/' + userId + 'keeps')
-    logger.log(res.data)
+    const res = await api.get('api/profiles/' + userId + '/keeps')
+    // logger.log("keeps", res.data)
     AppState.profileKeeps = res.data
   }
 
   async getVaultsByProfileId(userId) {
     const res = await api.get('api/profiles/' + userId + '/vaults')
-    logger.log(res.data)
+    // logger.log("vaults", res.data)
     AppState.profileVaults = res.data
   }
 }
+
 
 export const profilesService = new ProfilesService()
