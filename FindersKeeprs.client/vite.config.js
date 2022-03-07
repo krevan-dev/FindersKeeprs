8b8c34fc1654902dee8108ca03b8ea9ef4dcff57
Kyle Evans <krevan88@protonmail.com>
2022-03-07 15:46:51 -0700

    push it real good

--- FindersKeeprs.client/vite.config.js
+++ FindersKeeprs.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../FindersKeeprs/wwwroot',
     sourcemap: false
   },
   server: {
